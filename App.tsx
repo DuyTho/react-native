3f9b7b1601773cd41e710851d5493181ccf8c58b
Duy Tho <duythopm123@gmail.com>
2024-08-27 14:33:18 +0700

    views-text-styles

--- App.tsx
+++ App.tsx
@@ -4,9 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World with Duy Thọ</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <Text style={styles.header}>DuyTho</Text>
+        <Text style={styles.dad}>
+          I'm starting to learn React Native.
+          <Text style={styles.child}> I will conquer it!</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}>HELOOOOOOOOOOOO!</Text>
+      <Text>Hello World Duy Tho</Text>
+    </View >
   );
 }
 
@@ -17,4 +24,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: 'red',
+    fontWeight: 'bold',
+    padding: 10,
+    fontSize: 20
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: 'green'
+  },
+  dad: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: 'yellow'
+  },
+  child: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: 'blue'
+  }
 });
